cfcffcc0359f99a48f652ff53049f23583a1740a
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-02-24 01:05:06 +0530

    Final

---
+++ calculator/src/App.js
@@ -0,0 +1,131 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const handleEqual = () => {
+    if (isNaN(input)) {
+      setInput("NaN");
+    } else {
+      setInput(eval(input));
+    }
+    setResult(eval(input));
+    setInput(input);
+  };
+  const handleClear = () => {
+    setInput("");
+    setResult("");
+  };
+
+  return (
+    <div className="App">
+      <h1>React Calculator</h1>
+
+      <div>
+        <input value={input} className="calculator-input" />
+        <p>{result}</p>
+        <div className="rapper">
+          <input
+            type="button"
+            className="btns"
+            value="7"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="8"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="9"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="+"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="4"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="5"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="6"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="-"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="1"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="2"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="3"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="*"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="C"
+            onClick={handleClear}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="0"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="="
+            onClick={handleEqual}
+          />
+          <input
+            type="button"
+            className="btns"
+            value="/"
+            onClick={(e) => setInput(input + e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
